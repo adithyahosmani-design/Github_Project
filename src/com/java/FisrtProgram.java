2825ec41beba359e922cac25192e7e5dd1b10102
Aditya Hosamani <adithyahosmani@gmail.com>
2026-04-24 12:27:40 +0530

    kkhi

--- src/com/java/FisrtProgram.java
+++ src/com/java/FisrtProgram.java
@@ -4,10 +4,17 @@ public class FisrtProgram {
 	void print() {
 		System.out.println("Welcome to my github session 2");
 		System.out.println("hello guys");
+		
+	}
+	int add(int a,int b ) {
+		return a+b;
 	}
 	public static void main(String[] args) {
 		FisrtProgram fp=new FisrtProgram();
 		fp.print();
+		
+		int result = fp.add(10,10);
+		System.out.println("sum"+result);
 		
 	}
 

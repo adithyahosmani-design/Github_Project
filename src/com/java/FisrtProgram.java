265b7a207599de57fd62ccd4399294707e3aea36
Aditya Hosamani <adithyahosmani@gmail.com>
2026-04-24 12:14:39 +0530

    Addded new line

---
+++ src/com/java/FisrtProgram.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class FisrtProgram {
+	void print() {
+		System.out.println("Welcome to my github session 2");
+		System.out.println("hello guys");
+	}
+	public static void main(String[] args) {
+		FisrtProgram fp=new FisrtProgram();
+		fp.print();
+		
+	}
+
+}
--- src/com/java/firstProgram.java
+++
@@ -1,9 +0,0 @@
-package com.java;
-
-public class firstProgram {
-	void print()
-	{
-		System.out.println("Welcome back to Github session2");
-	}
-
-}
